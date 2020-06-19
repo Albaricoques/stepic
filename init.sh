1fbc0eff8ac08afc2adcca2223a14054ef729cfd
Albaricoques <67089994+Albaricoques@users.noreply.github.com>
2020-06-19 19:05:26 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,9 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo rm -rf /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart 
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+#sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
 
-﻿sudo /etc/init.d/mysql start﻿
+sudo /etc/init.d/mysql start

65e333d39bcfd11a076aae5819f736e5bd29c191
Albaricoques <67089994+Albaricoques@users.noreply.github.com>
2020-06-19 17:13:43 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-#sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+
 sudo /etc/init.d/nginx restart
 
+sudo rm -rf /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
